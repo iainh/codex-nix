ecf0fc76615b694fa3c77c47dcee5e1013cef66a
Iain H <iain@spiralpoint.org>
2025-10-31 20:17:49 -0400

    Update to 0.53.0

--- package.nix
+++ package.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   pname = "codex";
-  version = "0.47.0";
+  version = "0.53.0";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@openai/codex/-/codex-${version}.tgz";
-    sha256 = "0hbliyqah8hhs4ljpmajhiwbiggldi064r5k018n87pm8q75hpik";
+    sha256 = "1nnmm1wd4w2srvvj2783x2z56dzyxx7n7bkigji2sx7gglsbi0ib";
   };
 
   nativeBuildInputs = [ nodejs_22 makeWrapper ];
